51a280eb359bbad9514fc82be147e3c0add6ade1
ohtwo <hello@tinygram.dev>
2025-08-08 20:12:01 +0900

    1-11

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php
+$books = [
+    [
+        'name' => "Do Androids Dream of Electric Sheep",
+        'author' => "Philip K. Dick",
+        'purchaseUrl' => 'https://www.amazon.com/Androids-Dream-Electric-Sheep-inspiration/dp/1780220383/',
+        'releaseYear' => 1968
+    ],
+    [
+        'name' => 'The Langoliers',
+        'author' => "Stephen King",
+        'purchaseUrl' => 'https://www.amazon.com/Langoliers-Stephen-King/dp/1982136057',
+        'releaseYear' => 1995
+    ],
+    [
+        'name' => "Project Hail Mary",
+        'author' => "Andy Weir",
+        'purchaseUrl' => 'https://www.amazon.com/Project-Hail-Mary-Andy-Weir/dp/0593135202/',
+        'releaseYear' => 2021
+    ],
+];
+
+$filteredBooks = array_filter($books, function($book) {
+    return $book['author'] === 'Andy Weir';
+});
+
+require "about.view.php";
