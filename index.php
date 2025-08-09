5f4886b81a6b0a3487706717d1e0a82f45389cfe
ohtwo <hello@tinygram.dev>
2025-08-09 19:30:57 +0900

    1-17

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 
 require "functions.php";
-require "router.php";
+//require "router.php";
+
+// Connect to MySQL database.
+$host = 'localhost';
+$port = 3306;
+$dbname = 'myapp';
+$charset = 'utf8mb4';
+$username = 'root';
+$password = '';
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
