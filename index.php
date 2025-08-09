cdc1b0efa63e9f9bea685c13c8f10ca0c48708f9
ohtwo <hello@tinygram.dev>
2025-08-09 21:58:52 +0900

    1-20

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ $config = require("config.php");
 
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
