0455e80d9881114138c9cfef0dd776c837284a1c
ohtwo <hello@tinygram.dev>
2025-08-10 23:09:42 +0900

    1-25

--- views/partials/head.php
+++ views/partials/head.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+<!--    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>-->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
 <body class="h-full">
