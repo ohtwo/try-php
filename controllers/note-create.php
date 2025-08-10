c3eaa69f65b6fd8db25b9fa21f538885ddcb3344
ohtwo <hello@tinygram.dev>
2025-08-11 00:26:18 +0900

    1-26

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,15 @@
 <?php
 
+$config = require("config.php");
+$db = new Database($config['database']);
+
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO notes (user_id, body) VALUES (:user_id, :body)', [
+        'user_id' => 1,
+        'body' => $_POST['body'],
+    ]);
 }
 
 require "views/note-create.view.php";
