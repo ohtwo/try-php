5dd57d51598d781cf670bb02b56b46f4c774a611
ohtwo <hello@tinygram.dev>
2025-08-11 01:43:20 +0900

    1-28

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,6 +1,8 @@
 <?php
 
-$config = require("config.php");
+require 'Validator.php';
+
+$config = require"config.php";
 $db = new Database($config['database']);
 
 $heading = 'Create Note';
@@ -8,12 +10,8 @@ $heading = 'Create Note';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'Body cannot be empty';
-    }
-
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'Body cannot be longer than 1000 characters';
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required';
     }
 
     if (empty($errors)) {
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -26,7 +26,6 @@
                 </div>
 
                 <div class="mt-6 flex items-center justify-end gap-x-6">
-                    <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
                     <button type="submit"
                             class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         Save
@@ -37,3 +36,4 @@
     </main>
 
 <?php require('partials/footer.php') ?>
+
