2fccf11eb81155f1c968266a0d4fd516dc59d7e2
ohtwo <hello@tinygram.dev>
2025-08-11 01:09:01 +0900

    1-27

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,22 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes (user_id, body) VALUES (:user_id, :body)', [
-        'user_id' => 1,
-        'body' => $_POST['body'],
-    ]);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'Body cannot be empty';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'Body cannot be longer than 1000 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes (user_id, body) VALUES (:user_id, :body)', [
+            'user_id' => 1,
+            'body' => $_POST['body'],
+        ]);
+    }
 }
 
 require "views/note-create.view.php";
