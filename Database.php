71a25f39a1dfff884f4f39b91c266e688e18bf04
ohtwo <hello@tinygram.dev>
2025-08-10 21:49:37 +0900

    1-24

--- Database.php
+++ Database.php
@@ -3,7 +3,10 @@
 class Database
 {
     public $connection;
-    public function __construct($config, $username = 'root', $password = '') {
+    public $statement;
+
+    public function __construct($config, $username = 'root', $password = '')
+    {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
         $this->connection = new PDO($dsn, $username, $password, [
@@ -13,9 +16,30 @@ class Database
 
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
-        $statement->execute($params);
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
 
-        return $statement;
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if (!$result) {
+            abort();
+        }
+
+        return $result;
     }
 }
