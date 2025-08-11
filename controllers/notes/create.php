ca2b3ea222a4a559980e1c90194c6297f33e5211
ohtwo <hello@tinygram.dev>
2025-08-11 20:29:56 +0900

    1-34

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,28 +1,6 @@
 <?php
 
-use Core\Database;
-use Core\Validator;
-
-$config = require base_path("config.php");
-$db = new Database($config['database']);
-
-$errors = [];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    if (! Validator::string($_POST['body'], 1, 1000)) {
-        $errors['body'] = 'A body of no more than 1,000 characters is required';
-    }
-
-    if (empty($errors)) {
-        $db->query('INSERT INTO notes (user_id, body) VALUES (:user_id, :body)', [
-            'user_id' => 1,
-            'body' => $_POST['body'],
-        ]);
-    }
-}
-
 view("notes/create.view.php", [
     'heading' => 'Create Note',
-    'errors' => $errors,
+    'errors' => [],
 ]);
---
+++ controllers/notes/store.php
@@ -0,0 +1,28 @@
+<?php
+
+use Core\Database;
+use Core\Validator;
+
+$config = require base_path("config.php");
+$db = new Database($config['database']);
+
+$errors = [];
+
+if (!Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required';
+}
+
+if (!empty($errors)) {
+    view("notes/create.view.php", [
+        'heading' => 'Create Note',
+        'errors' => $errors,
+    ]);
+}
+
+$db->query('INSERT INTO notes (user_id, body) VALUES (:user_id, :body)', [
+    'user_id' => 1,
+    'body' => $_POST['body'],
+]);
+
+header('location: /notes');
+exit();
