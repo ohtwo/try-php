39bd5ba712cc0882f8f8ef32a45439bd5b11ee96
ohtwo <hello@tinygram.dev>
2025-08-12 22:21:26 +0900

    1-40

--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <p>Hello. Welcom to the home page.</p>
+            <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcom to the home page.</p>
         </div>
     </main>
 
