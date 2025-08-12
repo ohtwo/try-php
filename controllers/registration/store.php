096fb424c1e1f5796b02bbe47b43afbff3e6d97b
ohtwo <hello@tinygram.dev>
2025-08-12 21:06:13 +0900

    1-38

---
+++ controllers/registration/store.php
@@ -0,0 +1,51 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+
+if (!Validator::email($email)) {
+    $errors['email'] = 'Please enter a valid email address';
+}
+
+if (!Validator::string($password, 8, 255)) {
+    $errors['password'] = 'Please enter a password between 8 and 255 characters';
+}
+
+if (!empty($errors)) {
+    return view('registration/create.view.php', [
+        'errors' => $errors,
+    ]);
+}
+
+$db = App::resolve(Database::class);
+
+$user = $db->query('SELECT * FROM users WHERE email = :email', [
+    'email' => $email,
+])->find();
+
+
+if ($user) {
+    $errors['email'] = 'This email address is already in use';
+
+    return view('registration/create.view.php', [
+        'errors' => $errors,
+    ]);
+} else {
+    $db->query('INSERT INTO users (email, password) VALUES (:email, :password)', [
+        'email' => $email,
+        'password' => $password,
+    ]);
+
+    $_SESSION['user'] = [
+        'email' => $email,
+    ];
+
+    header('location: /');
+    exit();
+}
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -15,7 +15,7 @@
                     </div>
                 </div>
             </div>
-            <div class="hidden md:block">
+            <div class="md:block">
                 <div class="ml-4 flex items-center md:ml-6">
                     <button type="button" class="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
                         <span class="absolute -inset-1.5"></span>
@@ -26,19 +26,17 @@
                     </button>
 
                     <!-- Profile dropdown -->
-                    <el-dropdown class="relative ml-3">
-                        <button class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                    <?php if ($_SESSION['user'] ?? false) : ?>
+                        <button type="button"
+                                class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
                             <span class="absolute -inset-1.5"></span>
                             <span class="sr-only">Open user menu</span>
-                            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
+                            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                 alt="" class="size-8 rounded-full outline -outline-offset-1 outline-white/10"/>
                         </button>
-
-                        <el-menu anchor="bottom end" popover class="w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline-1 outline-black/5 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden">Your profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden">Settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden">Sign out</a>
-                        </el-menu>
-                    </el-dropdown>
+                    <?php else : ?>
+                        <a href="/register" class="text-white hover:text-gray-400">Register</a>
+                    <?php endif ?>
                 </div>
             </div>
             <div class="-mr-2 flex md:hidden">
