a0d4955a4a672113d6c7fa844ffe08058a68bfd3
ohtwo <hello@tinygram.dev>
2025-08-14 00:57:16 +0900

    1-46

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -2,22 +2,39 @@
 
 namespace Http\Forms;
 
+use Core\ValidationException;
 use Core\Validator;
 
 class LoginForm
 {
-    protected  $errors = [];
-    public function validate($email, $password)
+    protected $errors = [];
+
+    public function __construct(public array $attributes)
     {
-        if (!Validator::email($email)) {
+        if (!Validator::email($attributes['email'])) {
             $this->errors['email'] = 'Please enter a valid email address';
         }
 
-        if (!Validator::string($password)) {
+        if (!Validator::string($attributes['password'])) {
             $this->errors['password'] = 'Please enter a valid password';
         }
+    }
 
-        return empty($this->errors);
+    public static function validate($attributes)
+    {
+        $instance = new static($attributes);
+
+        return $instance->failed() ? $instance->throw() : $instance;
+    }
+
+    public function throw()
+    {
+        ValidationException::throw($this->errors(), $this->attributes);
+    }
+
+    public function failed()
+    {
+        return count($this->errors);
     }
 
     public function errors()
@@ -28,5 +45,6 @@ class LoginForm
     public function error($field, $message)
     {
         $this->errors[$field] = $message;
+        return $this;
     }
 }
--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,27 +1,19 @@
 <?php
 
 use Core\Authenticator;
-use Core\Session;
 use Http\Forms\LoginForm;
 
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$form = new LoginForm();
-
-if ($form->validate($email, $password)) {
-    $auth = new Authenticator();
-
-    if ($auth->attempt($email, $password)) {
-        redirect('/');
-    } else {
-        $form->error('email', 'Please enter a valid email and password');
-    }
-}
-
-Session::flash('errors', $form->errors());
-Session::flash('old', [
+$form = LoginForm::validate($attributes = [
     'email' => $_POST['email'],
+    'password' => $_POST['password'],
 ]);
 
-return redirect('/login');
+$signedIn = (new Authenticator)->attempt($attributes['email'], $attributes['password']);
+
+if (!$signedIn) {
+    $form->error('email', 'Please enter a valid email and password')
+        ->throw();
+
+}
+
+redirect('/');
